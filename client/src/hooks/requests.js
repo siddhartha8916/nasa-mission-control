cd7e8c0b16a9f665f31259652579f31555b40f2c
siddhartha8916 <siddhartha8916@gmail.com>
2023-02-20 22:46:44 +0530

    first commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,27 @@
+const API_URL = "http://localhost:8000";
+
+async function httpGetPlanets() {
+  try {
+    const response = await fetch(`${API_URL}/planets`);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
